5055f6bbd532f52353b952c184ed4452c64de5e5
Jacob Mazur <jacobmazur@Jacobs-MacBook-Pro.local>
2020-09-16 18:49:42 -0400

    database connection

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-mongoose.connect("mongodb://localhost/movies", {
+let url = process.env.DB_URL;
+
+mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
