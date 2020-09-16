a489c8fb00e2c18646905f89b19b3eaa07611bcf
Jacob Mazur <jacobmazur@Jacobs-MacBook-Pro.local>
2020-09-16 18:19:05 -0400

    adding data

--- models/director.js
+++ models/director.js
@@ -8,7 +8,7 @@ const mongoose = require("../db/connection.js");
 
 const directorSchema = new mongoose.Schema({
   name: String,
-  directorId: Number,
+  image: String,
 });
 
 module.exports = mongoose.model("directors", directorSchema);
