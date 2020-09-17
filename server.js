b09b58cf479cdc0a271c2654084e139d62f8c589
Jacob Mazur <jacobmazur@Jacobs-MacBook-Pro.local>
2020-09-16 22:42:48 -0400

    connection issues

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // import
 const express = require("express");
 const cors = require("cors");
+const Directors = require("./models/director.js");
 
 //configure app
 const app = express();
@@ -12,6 +13,12 @@ app.use(cors());
 
 //enpoints
 app.get("/", (req, res) => res.send("M E R N O L I T H"));
+
+app.get("/directors", (req, res) => {
+  Directors.find((err, data) => {
+    err ? res.status(500).send(err) : res.status(200).send(data);
+  });
+});
 
 //listen
 
