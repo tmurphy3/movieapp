6ff1e07b34f278b4be7d7bf609ca8aa414ccadf0
Jacob Mazur <jacobmazur@Jacobs-MacBook-Pro.local>
2020-09-16 20:59:06 -0400

    deploy

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 // import
 const express = require("express");
+const cors = require("cors");
 
 //configure app
 const app = express();
 
 //middleware
 app.use(express.json());
+app.use(cors());
 
 //enpoints
 app.get("/", (req, res) => res.send("M E R N O L I T H"));
