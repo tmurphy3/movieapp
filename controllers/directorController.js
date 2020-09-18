c4baa7a1e952647684d6a05df84dc4d437d616de
Timothy Murphy <timothymurphy@Timothys-MacBook-Pro.local>
2020-09-17 23:31:24 -0400

    debugging

--- controllers/directorController.js
+++ controllers/directorController.js
@@ -1,12 +1,11 @@
 let Directors = require("../models/Director");
-let Films = require("../models/Film");
 
 module.exports = {
   all: (req, res) => {
     Directors.find({}).then((all) => res.json(all));
   },
   directorMovies: (req, res) => {
-    Directors.findOne({ id: req.params.id }).populate("film"),
+    Directors.findOne({ id: req.params.id }).populate("Film"),
       (err, doc) => {
         if (err) handleError(res, err.message, "failed to get director");
         res.status(200).json(doc);
