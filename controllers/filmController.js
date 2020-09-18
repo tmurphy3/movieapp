40e2cd2e517c8360577fe223c19313043a48c3bb
Timothy Murphy <timothymurphy@Timothys-MacBook-Pro.local>
2020-09-18 10:51:05 -0400

    routes

--- controllers/filmController.js
+++ controllers/filmController.js
@@ -7,4 +7,11 @@ module.exports = {
   oneFilm: (req, res) => {
     Films.findOne({ _id: req.params.id }).then((film) => res.json(film));
   },
+  createFilm: (req, res) => {
+    Films.create(req.body).then((film) => res.json(film));
+  },
+  deleteFilm: (req, res) => {
+    Films.findOneAndDelete({ _id: req.params.id }).then((deleted) =>
+      res.json(deleted)
+    );
 };
--- routes/filmRouter.js
+++ routes/filmRouter.js
@@ -6,5 +6,7 @@ router = express();
 
 router.get("/", filmController.all);
 router.get("/:id", filmController.oneFilm);
+router.post("/", directorController.createFilm);
+router.delete("/:id", directorController.deleteFilm);
 
 module.exports = router;
